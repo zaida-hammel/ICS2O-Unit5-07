3763e2c327ef4e3b4ab204b06c797e2fba76c831
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-11 12:39:16 -0400

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Zaida Hammel
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+"use strict";
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-JS/sw.js", {
+    scope: "/ICS2O-Unit5-07-JS/",
+  });
+}
+
+/**
+ * This function does multiplication.
+ */
+function myButtonClicked () {
+  
 }
